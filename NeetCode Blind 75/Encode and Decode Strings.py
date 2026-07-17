dbfe9f9ed8211553453794024757977efa05446f
0x0Tamim <mmtamimuzzaman@gmail.com>
2026-07-17 14:33:01 +0600

    Solve Encode and Decode Strings using length-prefixed encoding

--- NeetCode Blind 75/Encode and Decode Strings.py
+++ NeetCode Blind 75/Encode and Decode Strings.py
@@ -1,18 +1,33 @@
 class Solution:
 
     def encode(self, strs):
-        string = " ".join(strs)
-        return string
+        string = ""
 
+        for i in strs:
+            string += str(len(i))+"#"+i
+        return string
+    
     def decode(self, s):
-        newList = s.split()
+        newList = []
+        
+        i = 0
+        while i<len(s):
+            j = i
+
+            while s[j] != "#":
+                j+=1
+
+            length = int(s[i:j])
+            print(j)
+            word = s[j+1:j+length+1]
+            newList.append(word)
+            i = j+1+length
         return newList
     
 sol = Solution()
-encoded_string = sol.encode(["Hello World"])
+encoded_string = sol.encode(["Hello World", "i am eliot", "anderson"])
 print(encoded_string)
 decoded_strs = sol.decode(encoded_string)
 print(decoded_strs)
-
 
 
