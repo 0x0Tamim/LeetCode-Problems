db69d5c74c24cd3b07c91eb6f7dae76fc8116917
0x0Tamim <mmtamimuzzaman@gmail.com>
2026-07-20 22:03:49 +0600

    Optimize Product of Array Except Self to O(1) extra space using running prefix and suffix products

--- NeetCode Blind 75/Products of Array Except Self.py
+++ NeetCode Blind 75/Products of Array Except Self.py
@@ -1,32 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums):
-        pref=[0]*len(nums)
-        for i in range (len(nums)  ):
-            if i == 0:
-                pref[i] = 1
+        n = len(nums)
+        pref=1
+        ans = [1]*n
+        for i in range (n):
+            ans[i] = pref
+            pref *= nums[i]
+        
+        print(ans)
 
-            else:
-                pref[i] = nums[i-1] * pref[i-1]
+        suf = 1
+        for j in range(n-1,-1,-1):
 
-        suf = [0]*len(nums)
-        for j in range(len(nums)-1,-1,-1):
+            ans[j] *= suf
+            suf *= nums[j] 
 
-            if j == len(nums)-1 :
-                suf[j] = 1
-            else:
-                suf[j] = nums [j+1] * suf[j+1]
-
-
-        print(pref, suf)
-
-
-        ans =[0]*len(nums)
-        for k in range(len(nums)):
-            ans[k] = pref[k]*suf[k]
 
         return ans
     
 
 
 sol = Solution()
-print(sol.productExceptSelf([1,2,4,6]))
+print(sol.productExceptSelf([2,4,6,7,8]))
